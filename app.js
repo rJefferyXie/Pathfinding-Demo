c7a2e7fdc399a71ad29875edeabbc19824edcda5
JefferyXie <realdiverzed27@gmail.com>
2021-02-07 23:52:52 -0600

    Added more styling, made buttons interactive, mobile menu created.

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ function change_content(title, description, video) {
     }
 }
 
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
 
 
 
