152ebbd71492e28017a171a3dff262485f1213df
JefferyXie <realdiverzed27@gmail.com>
2021-02-19 13:47:11 -0600

    Changed videos to .mp4, changed formatting.

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-function change_content(title, description, video) {
-    var current_title = document.getElementById("video__header");
-    var current_description = document.getElementById("video__description");
-    var current_video = document.getElementById("video__playing");
-    if (current_title.innerHTML === title) {
-      return;
-    }
-    else {
-        current_title.innerHTML = title;
-        current_description.innerHTML = description;
-        current_video.src = "demo_videos/" + video;
-    }
+function scroll_to(section) {
+  var destination = document.querySelector(section).offsetTop - 85;
+  window.scroll({
+    top: destination, 
+    behavior: "smooth"
+  });
 }
 
 const menu = document.querySelector('#mobile-menu');
